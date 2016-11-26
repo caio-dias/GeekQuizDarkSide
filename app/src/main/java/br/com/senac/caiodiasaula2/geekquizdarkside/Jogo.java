036ba5ce93d122249890b0eb7de059443c583dc1
joaopaulo <paiol4@hotmail.com>
2016-11-26 15:41:14 -0200

    Logica de criacao dos cards do jogo

--- app/src/main/java/br/com/senac/caiodiasaula2/geekquizdarkside/Jogo.java
+++ app/src/main/java/br/com/senac/caiodiasaula2/geekquizdarkside/Jogo.java
@@ -1,13 +1,9 @@
 package br.com.senac.caiodiasaula2.geekquizdarkside;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
@@ -18,12 +14,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import org.json.JSONArray;
-
-import java.lang.reflect.Type;
 import java.util.List;
 
 import retrofit2.Call;
@@ -35,8 +25,11 @@ public class Jogo extends AppCompatActivity {
     private ViewGroup alternativas;
     private TextView titulo_pergunta;
     private SharedPreferences prefQuestoes;
+    private SharedPreferences prefArrayQuestoes;
     private SharedPreferences.Editor editorQuestoes;
+    private SharedPreferences.Editor editorArrayQuestoes;
 
+    private String[] ArrayQuestoes;
     private String CodEvento;
 
     @Override
@@ -52,25 +45,104 @@ public class Jogo extends AppCompatActivity {
         prefQuestoes = getSharedPreferences("questoes", MODE_PRIVATE);
         editorQuestoes = prefQuestoes.edit();
 
+        prefArrayQuestoes = getSharedPreferences("ArrayQuestoes", MODE_PRIVATE);
+        editorArrayQuestoes = prefQuestoes.edit();
+
         GetQuestoes gq = new GetQuestoes();
-        gq.callQuestoes();
 
-        int qtdQuestoes = prefQuestoes.getInt("QtdQuestoes", 0);
-
-
-
+        gq.execute();
 
     }
 
-    private void addItem(String textoDaPergunta, String idPergunta, String correta) {
+    private void addItem(String textoDaPergunta, final String idPergunta, String correta,
+                         String codigoAlternativa) {
         CardView cardView = (CardView) LayoutInflater.from(this).inflate(R.layout.card_message, alternativas, false);
-        TextView alternativa = (TextView) cardView.findViewById(R.id.alternativa);
-        TextView idPerguntaTexto = (TextView) cardView.findViewById(R.id.idAlternativa);
-        TextView corretaTexto = (TextView) cardView.findViewById(R.id.correta);
+        final TextView alternativa = (TextView) cardView.findViewById(R.id.alternativa);
+        final TextView idPerguntaTexto = (TextView) cardView.findViewById(R.id.idAlternativa);
+        final TextView corretaTexto = (TextView) cardView.findViewById(R.id.correta);
+        final TextView codAlternativa = (TextView) cardView.findViewById(R.id.codAlternativa);
+        idPerguntaTexto.setText(idPergunta);
+        corretaTexto.setText(correta);
         alternativa.setText(textoDaPergunta);
+        alternativa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast
+                        .makeText(
+                                Jogo.this,
+                                "Voce Clicou " + corretaTexto.getText().toString(),
+                                Toast.LENGTH_LONG)
+                        .show();
+
+                SharedPreferences pref = getSharedPreferences("info", MODE_PRIVATE);
+                final String CodEvento = pref.getString("codEvento", "");
+                String codGrupo = "174";
+                InsereResposta ins = new InsereResposta();
+                ins.setCodAlternativa(idPerguntaTexto.getText().toString());
+                ins.setCodGrupo(codGrupo);
+                ins.setCodQuestao(codAlternativa.getText().toString());
+                ins.setCorreta(corretaTexto.getText().toString());
+                ins.setTempo("00:24");
+                ins.setTextoResp(alternativa.getText().toString());
+
+                //ins.doInBackground();
+
+
+
+
+
+            }
+        });
         idPerguntaTexto.setText(idPergunta);
         corretaTexto.setText(correta);
         alternativas.addView(cardView);
+    }
+
+    private void RequestQuestoes(){
+        int teste;
+
+        teste = 0;
+
+        GetQuestoesPerguntas getQ = new GetQuestoesPerguntas();
+        getQ.doInBackground();
+
+    }
+
+    private void MontaCards(){
+        int OrdemPergunta = 0;
+        int qtdQuestoes = prefQuestoes.getInt("QtdQuestoes", 0);
+        String PerguntaSring;
+        String Simbolo = "#";
+        int ControleQuestao = 0;
+
+        PerguntaSring = prefQuestoes.getString("Questao"+OrdemPergunta, "");
+        String[] perguntas =  PerguntaSring.split(Simbolo);
+        ControleQuestao = Integer.parseInt(perguntas[2].toString());
+        titulo_pergunta = (TextView) findViewById(R.id.titulo_pergunta);
+        titulo_pergunta.setText(perguntas[0].toString());
+
+        try{
+        if(qtdQuestoes > 0){
+
+            while(ControleQuestao ==  Integer.parseInt(perguntas[2].toString()))
+            {
+                OrdemPergunta++;
+                PerguntaSring = prefQuestoes.getString("Questao"+OrdemPergunta, "");
+                perguntas =  PerguntaSring.split(Simbolo);
+
+                addItem(perguntas[1].toString(), perguntas[2].toString(), perguntas[3].toString(),
+                        perguntas[4].toString());
+
+                ControleQuestao = Integer.parseInt(perguntas[2].toString());
+                editorQuestoes.putInt("OrdemPergunta", OrdemPergunta);
+                editorQuestoes.apply();
+            }
+        }
+        }catch (Exception e){
+
+        }
+
+
     }
 
     public class GetQuestoes extends AsyncTask<Void, Void, String>
@@ -96,51 +168,31 @@ public class Jogo extends AppCompatActivity {
                             List<QuestoesEvento> questoes = response.body();
                             int QuestoesTotais =  questoes.size();
 
-                            editorQuestoes.putInt("QtdQuestoes", QuestoesTotais);
-                            editorQuestoes.apply();
-
-                            for (int i = 0; i < questoes.size(); i++)
+                            ArrayQuestoes = new String[QuestoesTotais];
+                            for (int i = 0; i < questoes.size()-1; i++)
                             {
-                                final Call<Questoes> callDaQuestoes = service.JsonAlternativaQUESTOES(CodEvento, questoes.get(i).getCodQuestao());
-                                final int indice = i;
-                                callDaQuestoes.enqueue(new Callback<Questoes>() {
-                                    @Override
-                                    public void onResponse(Call<Questoes> call, Response<Questoes> response) {
-                                        if (response.isSuccessful()) {
-                                            final Questoes questoesEvento = response.body();
-
-
-                                                editorQuestoes.putString("Questao" + indice, questoesEvento.getTextoquestao().toString() + "#" +  questoesEvento.getTextoalternativa().toString()
-                                                        + "#"+  questoesEvento.getCodalternativa().toString()
-                                                        + "#"+ questoesEvento.getCorreta());
-                                            editorQuestoes.apply();
-
-
-                                        }
-                                    }
-
-                                    @Override
-                                    public void onFailure(Call<Questoes> call, Throwable t) {
-                                        Log.d("hh","k");
-                                    }
-                                });
-
-
-                                //titulo_pergunta = (TextView) findViewById(R.id.titulo_pergunta);
-
-                                //titulo_pergunta.setText(questoes.get(i).getTEXTOQUESTAO().toString());
+                                try {
+                                    ArrayQuestoes[i] = questoes.get(i).getCodQuestao();
+                                }catch (Exception e)
+                                {
+                                    e.printStackTrace();
+                                }
 
                             }
+
+                                RequestQuestoes();
+
 
                         } else {
                             Toast
                                     .makeText(
                                             Jogo.this,
-                                            "Nao foi possivel logar",
+                                            "Nao foi possivel criar as perguntas",
                                             Toast.LENGTH_LONG)
                                     .show();
 
                         }
+
                     }
                     @Override
                     public void onFailure(Call<List<QuestoesEvento>> call, Throwable t) {
@@ -151,7 +203,148 @@ public class Jogo extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+            return "done";
+        }
+
+
+    }
+
+    public  class GetQuestoesPerguntas extends AsyncTask<Void, Void, Void>
+    {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            SharedPreferences pref = getSharedPreferences("info", MODE_PRIVATE);
+            final String CodEvento = pref.getString("codEvento", "");
+
+            Retrofit retrofit = Rest.getInstance().get();
+            final RestEndPointDarkSide service = retrofit.create(RestEndPointDarkSide.class);
+            String Opcao = "1";
+            final Call<List<Questoes>> callDaQuestoes = service.JsonAlternativaQUESTOES(CodEvento, Opcao);
+
+            callDaQuestoes.enqueue(new Callback<List<Questoes>>() {
+                @Override
+                public void onResponse(Call<List<Questoes>> call, Response<List<Questoes>> response) {
+                    if (response.isSuccessful()) {
+                        List<Questoes> questoesEvento = response.body();
+
+                        for (int i = 0; i < questoesEvento.size(); i++) {
+
+                            addItem(questoesEvento.get(i).getTextoalternativa(),
+                                    questoesEvento.get(i).getCodquestao(), questoesEvento.get(i).getCorreta(),
+                                    questoesEvento.get(i).getCodalternativa());
+
+
+                            titulo_pergunta = (TextView) findViewById(R.id.titulo_pergunta);
+                            titulo_pergunta.setText(questoesEvento.get(i).getTextoquestao().toString());
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Questoes>> call, Throwable t) {
+                    Log.d("hh","k");
+                }
+            });
+
             return null;
+        }
+    }
+
+    public class InsereResposta extends AsyncTask<Void, Void, Void>{
+
+        private String codQuestao;
+        private String codAlternativa;
+        private String codGrupo;
+        private String tempo;
+        private String textoResp;
+        private String correta;
+
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try{
+            Retrofit retrofit = Rest.getInstance().get();
+            final RestEndPointDarkSide service = retrofit.create(RestEndPointDarkSide.class);
+            final Call<Void> callDa = service.InsereResposta(getCodQuestao(), getCodAlternativa(),
+                    getCodGrupo(), getTempo(), getTextoResp(), getCorreta());
+            callDa.enqueue(new Callback<Void>() {
+                @Override
+                public void onResponse(Call<Void> call, Response<Void> response) {
+                    if (response.isSuccessful()) {
+                        String Resp = response.body().toString();
+                        RequestQuestoes();
+                    } else {
+                        Toast
+                                .makeText(
+                                        Jogo.this,
+                                        "Nao foi possivel criar as perguntas",
+                                        Toast.LENGTH_LONG)
+                                .show();
+                    }
+                }
+                @Override
+                public void onFailure(Call<Void> call, Throwable t) {
+                    Log.d("hh","k");
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+        }
+
+        public String getCodQuestao() {
+            return codQuestao;
+        }
+
+        public void setCodQuestao(String codQuestao) {
+            this.codQuestao = codQuestao;
+        }
+
+        public String getCodAlternativa() {
+            return codAlternativa;
+        }
+
+        public void setCodAlternativa(String codAlternativa) {
+            this.codAlternativa = codAlternativa;
+        }
+
+        public String getCodGrupo() {
+            return codGrupo;
+        }
+
+        public void setCodGrupo(String codGrupo) {
+            this.codGrupo = codGrupo;
+        }
+
+        public String getTempo() {
+            return tempo;
+        }
+
+        public void setTempo(String tempo) {
+            this.tempo = tempo;
+        }
+
+        public String getTextoResp() {
+            return textoResp;
+        }
+
+        public void setTextoResp(String textoResp) {
+            this.textoResp = textoResp;
+        }
+
+        public String getCorreta() {
+            return correta;
+        }
+
+        public void setCorreta(String correta) {
+            this.correta = correta;
         }
     }
 }
